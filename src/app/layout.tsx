412e3daf7cbce44ee38f54918eaddb13b6c6ed68
YamaD <me@dyama.net>
2023-10-22 23:18:51 +0900

    add eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import { Inter } from "next/font/google"
 import "./global.css"
+
+import { Metadata } from "next"
+import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Launcher",
   description: "org.kobe-kosen.kcctfes23.launcher",
 }
